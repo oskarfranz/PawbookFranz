97afdbe8aaee23abbcee2141574b0a3746b50a1d
oskarfranz <71341673+oskarfranz@users.noreply.github.com>
2022-04-26 19:31:07 -0500

    Implementación de filtros
    Los filtros en tiempo real de los perrillos ya funcionan.
    
    Solamente falta implementar el filtro de Perro/Gato porque las mascotas no tienen un atributo que los identifique como tal

--- FRONTEND/src/app/pages/pets/pets.component.ts
+++ FRONTEND/src/app/pages/pets/pets.component.ts
@@ -12,6 +12,8 @@ export class PetsComponent implements OnInit {
   razaSelected: boolean = false;
   private petService;
   pets: Pet[] = [];
+  allPets: Pet[] = []
+  private filters: String[] = [];
 
   constructor(private router: Router, petService: PetService) {
     this.petService = petService;
@@ -21,10 +23,54 @@ export class PetsComponent implements OnInit {
     this.petService.getPets().subscribe(response => {
       console.log('Response: ', response);
       this.pets = response;
+      this.allPets = this.pets;
     })
   }
   moreInfo(){
     this.router.navigate(['/pet']);
+  }
+
+  addFilter(filter: String){
+    if(this.filters.indexOf(filter) != -1){ //Si ya existía el filtro en la lista, lo removemos
+      var updatedFilters: String[] = this.filters.filter(function(value, index, arr){ 
+        return value != filter;
+       });
+      this.filters = updatedFilters;
+    }
+    else{                                   //Si no existía en la lista, lo añadimos
+      this.filters.push(filter);
+    }
+    console.log("active filters: " + this.filters);
+    this.updateVisiblePets(this.filters);
+  }
+
+  updateVisiblePets(filters: String[]){
+    this.pets =  this.allPets; //Inicializamos sin filtros
+
+    // Caso base
+    if(filters.length == 0) return;
+    var mascotas: Pet[] = this.pets;
+    var updatedPets: Pet[] = [];
+    filters.forEach(function(filtro, indice, arreglo) {
+      mascotas.forEach(function(mascota, index, array) {
+        // console.log(mascota.size + "  " + filtro);
+        if(filtro == mascota.size){
+          console.log(mascota);
+          updatedPets.push(mascota);
+        }
+
+        if((filtro == '0-2' && mascota.age <= 2) || 
+           (filtro == '3-6' && (mascota.age <= 6 && mascota.age >= 3)) || 
+           (filtro == '7-9' && (mascota.age <= 9 && mascota.age >= 7)) || 
+           (filtro == '9+'  && (mascota.age >= 9))){
+
+          console.log(mascota);
+          updatedPets.push(mascota);
+          
+        }
+      });
+    });
+    this.pets = updatedPets;
   }
 
 
