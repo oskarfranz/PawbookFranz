a58e56bf5d85c46a5482bda44c01d3538ba7e92d
oskarfranz <71341673+oskarfranz@users.noreply.github.com>
2022-05-02 10:40:10 -0500

    .

--- FRONTEND/src/app/pages/modal/modal.component.ts
+++ FRONTEND/src/app/pages/modal/modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { UserService } from 'src/app/shared/services/user.service';
+
 
 @Component({
   selector: 'app-modal',
@@ -7,15 +9,42 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
-  applications = [];
+  applications: any  = [];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  allUsers: any = [];
+  users : any = [];
+
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,  private userService: UserService) { }
+
+  findUser(email: any, emailToCompare: any) {
+    return email ===  emailToCompare;
+}
 
   ngOnInit(): void {
     if(this.data != undefined){
-      this.applications = this.data;
+
+      this.userService.getUsers().subscribe(response => {
+        // console.log(response)
+        this.allUsers = response;
+        
+        let i =0;
+        for(i=0;i<=this.data.length;i++){
+  
+          let j=0;
+  
+          for(j=0;j<this.allUsers.length;j++){
+            if(this.allUsers[j].email === this.data[i]){
+              // console.log(this.data[i] + " : " + this.allUsers[j].email);
+              this.users.push(this.allUsers[j])
+            }
+          }
+        }
+        
+      })
+      // this.applications = this.data;
     }
-    console.log(this.applications);
+    console.log(this.users);
   }
 
 }
