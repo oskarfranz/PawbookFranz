6e70e0bb82b268bd9a4de641c8a3b33c6038ee51
1929sofiaArceo <sofiaarceo.cov@gmail.com>
2022-05-12 15:25:47 -0500

    file managment backend complete

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -8,7 +8,7 @@ const socketIo = require('socket.io');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
-
+const multer = require('multer');
 
 //incializamos app
 const app = express();
@@ -16,6 +16,40 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', apiRoutes);
 
+// Manejo de archivos con multer
+const multerOptions = {
+    destination: (req, file, cb)=>{
+        cb(null, 'storage/images'); //error first callback
+    },
+    filename: (req, file, cb)=>{
+        const extension = file.originalname.split('.').pop(); //extraigo la extension
+        const fileName = file.originalname.split('.')[0];
+        cb(null, fileName+'.'+extension); //nombre con el que lo quiero guardar
+    }
+}
+const allowExtensions = ['jpg', 'png', 'jpeg', 'gif']; //como es para fotos de pets solo permitimos ciertas extensiones
+
+const fileFilter = (req, file, cb)=>{
+    console.log(file);
+    const extension = file.originalname.split('.').pop().toLowerCase();
+    const flag = file.mimetype.startsWith('image/');
+    cb(null, flag);
+}
+
+const multerStorage = multer.diskStorage(multerOptions);
+
+const upload = multer({storage: multerStorage, fileFilter: fileFilter}); //creamos el middleware dond eespecificamos el storage
+
+app.post('/file', upload.single('archivo'),(req, res)=>{
+    console.log("Archivo: ", req.file);
+    if(!req.file){
+        console.log("no hay archivo");
+    }else{
+        res.status(200).send("subida de archivo");
+    }
+});
+
+//endopoints
 app.get('/', (req, res) => {
     const indexPath = path.join(__dirname, 'src/views/', 'index.html');
     res.sendFile(indexPath);
